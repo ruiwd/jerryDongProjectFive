af5e29024b107ae44eb4016ef75da3e2bdb2d62f
Jerry Dong <rtdong9421@yahoo.com>
2020-06-22 11:39:49 -0400

    Updates

---
+++ precache-manifest.1a9ec5ca86afbebb57b1272a6852a060.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8935c3520c3c89dfe6c5036807f39bc3",
+    "url": "/jerryDongProjectFive/index.html"
+  },
+  {
+    "revision": "ace327acfcd2a99aaa62",
+    "url": "/jerryDongProjectFive/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "949ee2bff249fc264898",
+    "url": "/jerryDongProjectFive/static/js/2.616543b7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jerryDongProjectFive/static/js/2.616543b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ace327acfcd2a99aaa62",
+    "url": "/jerryDongProjectFive/static/js/main.24ed6069.chunk.js"
+  },
+  {
+    "revision": "eaefa4320d5c503e2e9c",
+    "url": "/jerryDongProjectFive/static/js/runtime-main.4b40ed05.js"
+  }
+]);
